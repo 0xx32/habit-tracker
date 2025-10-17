95b0d10b9413937dbba4b60f084af1f986b363a6
0xx32 <dimas1523@mail.ru>
2025-10-17 22:09:01 +0300

    added package eslint-config

---
+++ packages/eslint-config/.prettierrc.js
@@ -0,0 +1,6 @@
+import prettierConfig from "@0x32/prettier";
+
+/**
+ * @type {import("prettier").Config}
+ */
+export default prettierConfig.default;
---
+++ packages/eslint-config/node.js
@@ -0,0 +1,38 @@
+import antfu from "@antfu/eslint-config";
+import perfectionist from "eslint-plugin-perfectionist";
+
+export const nodeConfig = antfu(
+  {
+    stylistic: false,
+    formatters: false,
+    typescript: true,
+  },
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          groups: [
+            "type",
+            ["builtin", "external"],
+            "internal-type",
+            ["internal"],
+            ["parent-type", "sibling-type", "index-type"],
+            ["parent", "sibling", "index"],
+            "object",
+            "style",
+            "side-effect-style",
+            "unknown",
+          ],
+          internalPattern: ["^~/.*", "^@/.*"],
+          newlinesBetween: "always",
+          order: "asc",
+          type: "natural",
+        },
+      ],
+    },
+  }
+);
