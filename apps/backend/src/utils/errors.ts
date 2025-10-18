6187d583ae70c53d432a5afb78af86fa6a4f47b5
0xx32 <dimas1523@mail.ru>
2025-10-19 01:39:34 +0300

    fix: appError class

--- apps/backend/src/utils/errors.ts
+++ apps/backend/src/utils/errors.ts
@@ -1,30 +1,34 @@
 import type { ErrorCode } from './constants/errors'
 
 export class AppError extends Error {
+	public statusCode: number
+
 	constructor(
 		message: string,
+		statusCode: number,
 		public code?: ErrorCode
 	) {
 		super(message)
 		this.name = this.constructor.name
+		this.statusCode = statusCode
 	}
 }
 
 export class ConflictError extends AppError {
 	constructor(message: string, code?: ErrorCode) {
-		super(message, code)
+		super(message, 400, code)
 	}
 }
 
 export class NotFoundError extends AppError {
 	constructor(message: string, code?: ErrorCode) {
-		super(message, code)
+		super(message, 404, code)
 	}
 }
 
 export class InternalServerError extends AppError {
 	constructor(message: string, code?: ErrorCode) {
-		super(message, code)
+		super(message, 500, code)
 	}
 }
 
